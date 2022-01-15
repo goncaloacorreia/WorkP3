e69caefca8a72cb43a75ac1ef4bd9841d6bf8d1f
goncaloacorreia <gac632000@gmail.com>
2022-01-15 21:10:13 +0000

    final

--- strimko.pl
+++ strimko.pl
@@ -14,7 +14,7 @@ converter(L,R) :- accC(L,R).
 accC([],[]).
 accC([H|T1],[H1|T2]) :- converter0(H, H1), accC(T1,T2).
 
-%converter o input
+%converter o input para inteiros
 converterInput(Y):- read1(L), converter(L, Y).
 
 %preencher o tabuleiro S com os dados do array
@@ -44,34 +44,35 @@ readStreams(Y) :- readSaux(L), append(_, Y, L), length(Y, 4).
 diff(V) :- V ins 1..4, all_distinct(V).
 
 %execução
-goaux( [[P11,P12,P13,P14],
-[P21,P22,P23,P24],
-[P31,P32,P33,P34],
-[P41,P42,P43,P44]] ) :-
+goaux( [[P11,P12,P13,P14], %tabuleiro
+        [P21,P22,P23,P24],
+        [P31,P32,P33,P34],
+        [P41,P42,P43,P44]] ) :-
 
     %linhas
-    diff([P11,P12,P13,P14]),
-    diff([P21,P22,P23,P24]),
-    diff([P31,P32,P33,P34]),
-    diff([P41,P42,P43,P44]),
+    diff([P11,P12,P13,P14]), %primeira linha
+    diff([P21,P22,P23,P24]), %segunda linha
+    diff([P31,P32,P33,P34]), %terceira linha
+    diff([P41,P42,P43,P44]), %quarta linha
 
     %colunas
-    diff([P11,P21,P31,P41]),
-    diff([P12,P22,P32,P42]),
-    diff([P13,P23,P33,P43]),
-    diff([P14,P24,P34,P44]),
+    diff([P11,P21,P31,P41]), %primeira coluna
+    diff([P12,P22,P32,P42]), %segunda coluna
+    diff([P13,P23,P33,P43]), %terceira coluna
+    diff([P14,P24,P34,P44]), %quarta coluna
 
     %streams
-    diff([P11,P22,P33,P44]),
-    diff([P21,P12,P13,P24]),
-    diff([P31,P42,P43,P34]),
-    diff([P41,P32,P23,P14]),
-
-    %labelling
+    diff([P11,P22,P33,P44]), %primeira stream
+    diff([P21,P12,P13,P24]), %segunda stream
+    diff([P31,P42,P43,P34]), %terceira stream
+    diff([P41,P32,P23,P14]), %quarta stream
+   
+    
+    %labelling (fazer com que cada variável tenha um valor assigned)
     labeling([],[P11,P12,P13,P14,P21,P22,P23,P24,
     P31,P32,P33,P34,P41,P42,P43,P44]),
 
-    %output
+    %output(print da resolução)
     write(P11), write(' '), write(P12), write(' '),
     write(P13), write(' '), write(P14), nl,
     write(P21), write(' '), write(P22), write(' '),
